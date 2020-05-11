a8e063a9d39ddf280fd6338dc74f9705f3fba531
Trevor Osborne <trevor.osborne113@gmail.com>
2020-05-11 09:35:30 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator-ng';
+  num1 = 0;
+  num2 = 0;
+  result = 0;
+
+  add():void {
+    this.result = this.num1 + this.num2;
+  }
+  
+  subtract():void {
+    this.result = this.num1 - this.num2;
+  }
+
+  multiply():void {
+    this.result = this.num1 * this.num2;
+  }
+
+  divide():void {
+    this.result = this.num1 / this.num2;
+  }
+
 }
